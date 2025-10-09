f5e09b7f26c3450ce26bbf02259175fd7b9f6287
Muhammadjon <zoirovm823@gmail.com>
2025-10-10 04:55:02 +0500

    integrate vslidation pipes logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -4,7 +4,7 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel("Member") private readonly memberModel: Model<null>){}
+    constructor(@InjectModel("Member") private readonly memberModel: Model<null>) { }
     public async signup(): Promise<string> {
         return 'Signup executed'
     }
@@ -21,4 +21,3 @@ export class MemberService {
         return 'getMember executed'
     }
 }
- 
