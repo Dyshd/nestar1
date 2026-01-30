f3891a870c21629f158d30df224db698d30c7d64
Muhammadjon <zoirovm823@gmail.com>
2026-01-30 10:02:53 +0500

    fix: modify WebSocket Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -1,21 +1,33 @@
 import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
-import * as WebSocket from "ws"
+import * as WebSocket from 'ws';
+import { AuthService } from '../components/auth/auth.service';
+import { Member } from '../libs/dto/member/member';
+import * as url from 'url';
+import { listenerCount } from 'process';
 
 interface MassagePayload {
   event: string;
   text: string;
+  memberData: Member | null;
 }
 
 interface InfoPayload {
   event: string;
   totalClients: number;
+  memberData: Member | null;
+  action: string;
 }
+
 @WebSocketGateway({ transports: ['websocket'], secure: false })
 export class SocketGateway implements OnGatewayInit {
   private logger: Logger = new Logger('SocketEventsGateway');
   private summaryClient: number = 0;
+  private clientsAuthMap = new Map<WebSocket, Member | null>();
+  private messagesList: MassagePayload[] = []
+
+  constructor(private authService: AuthService) { }
 
   @WebSocketServer()
   server: Server;
@@ -24,40 +36,76 @@ export class SocketGateway implements OnGatewayInit {
     this.logger.verbose(`WebSocket Server Initialized & total [${this.summaryClient}]`);
   }
 
-  handleConnection(client: WebSocket, ...args: any[]) {
+  private async retrieveAuth(req: any): Promise<Member | null> {
+    try {
+      const parseUrl = url.parse(req.url, true);
+      const { token } = parseUrl.query;
+      console.log('token:', token);
+      return await this.authService.verifyToken(token as string);
+    } catch (err) {
+      return null;
+    }
+  }
+
+  public async handleConnection(client: WebSocket, req: any) {
+    const authMember = await this.retrieveAuth(req);
+
     this.summaryClient++;
-    this.logger.verbose(`Connection & total [${this.summaryClient}]`);
+    this.clientsAuthMap.set(client, authMember);
+
+    const clientNick: string = authMember?.memberNick ?? 'Guest';
+    this.logger.verbose(`Connection[${clientNick}] & total [${this.summaryClient}]`);
+
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action: 'joined',
     };
-    this.emitMessage(infoMsg)
+
+    this.emitMessage(infoMsg);
   }
 
+  public handleDisconnect(client: WebSocket) {
+    const authMember = this.clientsAuthMap.get(client) ?? null;
 
-  handleDisconnect(client: WebSocket) {
     this.summaryClient--;
-    this.logger.verbose(`Disconnection & total [${this.summaryClient}]`);
+    this.clientsAuthMap.delete(client);
+
+    const clientNick: string = authMember?.memberNick ?? 'Guest';
+    this.logger.verbose(`Disconnection[${clientNick}] & total [${this.summaryClient}]`);
+
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action: 'left',
     };
-    this.broadcastMessage(client, infoMsg);
-  }
 
+    this.broadcastMessage(client, infoMsg);
+    client.send(JSON.stringify({ event: "getMessage", list: this.messagesList }))
+  }
   @SubscribeMessage('message')
   public async handleMessage(client: WebSocket, payload: string): Promise<void> {
-    const newMessage: MassagePayload = { event: 'message', text: payload };
-    this.logger.verbose(`NEW MESSAGE: ${payload}`);
+    const authMember = this.clientsAuthMap.get(client) ?? null;
+
+    const newMessage: MassagePayload = { event: 'message', text: payload, memberData: authMember };
+    const clientNick: string = authMember?.memberNick ?? 'Guest';
+
+    this.logger.verbose(`NEW MESSAGE [${clientNick}]: ${payload}`);
+    this.messagesList.push(newMessage);
+    if (this.messagesList.length > 5) this.messagesList.splice(0, this.messagesList.length - 5)
     this.emitMessage(newMessage);
   }
+
+
 
 
   private broadcastMessage(sender: WebSocket, message: InfoPayload | MassagePayload) {
     this.server.clients.forEach((client) => {
       if (client !== sender && client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(message));
-      } 
+      }
     });
   }
 
